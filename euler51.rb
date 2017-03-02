c7fd4cd58449dd794b2a70b7214cf80b7b5fbf06
Harrison McCullough <mccullough.harrison@gmail.com>
2017-03-02 16:07:36 -0700

    Made edits to problem 51.

--- euler51.rb
+++ euler51.rb
@@ -3,6 +3,7 @@
 require 'prime'
 
 module Enumerable
+
   def this_many? n, &block
     count = 0
     self.each { |x|
@@ -15,16 +16,32 @@ module Enumerable
   end
 
   def this_many_range? range, &block
+
     count = 0
-    in_range_fun = lambda { |c| not range.include? c }
-    @cur_fun = lambda { |c| @cur_fun = in_range_fun if c > range.begin; false }
-    self.each { |x|
-      if block.call x
-        count += 1
-        return false if @cur_fun.call count
+    e = self.each
+
+    while true
+      begin
+        if block.call e.next
+          count += 1
+          break if range.include? count
+        end
+      rescue StopIteration
+        return false
       end
-    }
-    return range.include?(count)
+    end
+
+    while true
+      begin
+        if block.call e.next
+          count += 1
+          return false if not range.include? count
+        end
+      rescue StopIteration
+        return range.include? count
+      end
+    end
+
   end
 
 end
@@ -32,7 +49,11 @@ end
 puts [1,2,3].this_many?(1) { |x| x < 3 }
 puts [1,2,3].this_many?(2) { |x| x < 3 }
 puts [1,2,3].this_many?(3) { |x| x < 3 }
+puts '[1,2,3](1..2)  { |x| x < 3 }'
 puts [1,2,3].this_many_range?(1..2) { |x| x < 3 }
+puts '[1,2,3](1...2) { |x| x < 3 }'
 puts [1,2,3].this_many_range?(1...2) { |x| x < 3 }
+puts '[1,2,3](1..3)  { |x| x < 3 }'
 puts [1,2,3].this_many_range?(1..3) { |x| x < 3 }
+puts '[1,2,3](3..4)  { |x| x < 3 }'
 puts [1,2,3].this_many_range?(3..4) { |x| x < 3 }
