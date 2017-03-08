f6f38b64a673cc69e50cc2e8ac74ae09a6c949dd
Harrison McCullough <mccullough.harrison@gmail.com>
2017-03-08 13:21:29 -0700

    Maybe solved problem 52?

--- euler52.rb
+++ euler52.rb
@@ -13,28 +13,42 @@ def max_valid_same_digits(k,num_digits)
 end
 
 def find_multiples(k,max_digits)
-  (1..).flat_map { |num_digits|
+  (1..max_digits).flat_map { |num_digits|
     max_valid = max_valid_same_digits(k,num_digits)
     min_valid = 10**(num_digits-1)
     (min_valid..max_valid).select { |x| same_digits x, k*x }
   }
 end
 
-max_digits = 5
-(2..6).each { |k|
-  puts "#{k}:"
-  commonxs = []
-  samex = (1..max_digits).flat_map { |num_digits|
-    max_valid = max_valid_same_digits(k,num_digits)
-    min_valid = 10**(num_digits-1)
-    # puts "#{num_digits} => (#{min_valid}..#{max_valid})"
-    (min_valid..max_valid).select { |x|
-      same_digits x, k*x
-    }
-  }
-  samex.each do |x|
-    puts "\t#{k}*#{x}=#{k*x}"
+# max_digits = 5
+# (2..6).each { |k|
+#   puts "#{k}:"
+#   commonxs = []
+#   samex = (1..max_digits).flat_map { |num_digits|
+#     max_valid = max_valid_same_digits(k,num_digits)
+#     min_valid = 10**(num_digits-1)
+#     # puts "#{num_digits} => (#{min_valid}..#{max_valid})"
+#     (min_valid..max_valid).select { |x|
+#       same_digits x, k*x
+#     }
+#   }
+#   samex.each do |x|
+#     puts "\t#{k}*#{x}=#{k*x}"
+#   end
+#   puts ""
+#   # p samex
+# }
+
+min_digits = 2
+max_digits = 6
+xs = (min_digits..max_digits).flat_map { |num_digits|
+  max_valid = max_valid_same_digits(6,num_digits)
+  min_valid = 10**(num_digits-1)
+  xs = (min_valid..max_valid).select { |x| same_digits x, 6*x }
+  (2..5).to_a.reverse.each do |k|
+    xs.select! { |x| same_digits x, k*x }
   end
-  puts ""
-  # p samex
+  xs
 }
+
+p xs
