628528ec6c7e37efc074134e7e52486f8112c978
Harrison McCullough <mccullough.harrison@gmail.com>
2017-03-10 16:47:12 -0700

    Got problem 54 to mostly work but haven't gotten the right answer yet.

--- euler54.rb
+++ euler54.rb
@@ -15,6 +15,7 @@ card_map = {"2"  =>  2,
             "A"  => 14}
 
 class Card
+  include Comparable
   @@card_map = {"2"  =>  2,
                 "3"  =>  3,
                 "4"  =>  4,
@@ -50,19 +51,19 @@ class Card
   end
 end
 
-c1 = Card.new("4C")
-puts c1.show, c1.value, c1.suit
-c2 = Card.new("3D")
-puts c1.show, c2.show, c1 <=> c2
-puts c2.show, c1.show, c2 <=> c1
-c3 = Card.new("3H")
-puts c3.show, c2.show, c3 <=> c2
-puts c2.show, c3.show, c2 <=> c3
-puts c3.show, c3.show, c3 <=> c3
-puts c3.show, c3.show, c3 <=> c3
-# exit
+# c1 = Card.new("4C")
+# puts c1.show, c1.value, c1.suit
+# c2 = Card.new("3D")
+# puts c1.show, c2.show, c1 <=> c2
+# puts c2.show, c1.show, c2 <=> c1
+# c3 = Card.new("3H")
+# puts c3.show, c2.show, c3 <=> c2
+# puts c2.show, c3.show, c2 <=> c3
+# puts c3.show, c3.show, c3 <=> c3
+# puts c3.show, c3.show, c3 <=> c3
 
 class Hand
+  include Comparable
   attr_reader :cards
   attr_reader :all_same_suit
   attr_reader :all_ascending
@@ -113,8 +114,6 @@ class Hand
     }.reverse
   end
   def score_hand
-    puts "score_hand {(#{self.show}), (#{self.groups[0].length}), " +
-         "(#{self.groups[1].length}), (#{self.groups})}\n\n---\n"
     if @all_same_suit and @all_ascending
       return [@@straight_flush, @card_values_by_value]
     elsif @groups[0][1].length >= 4
@@ -140,131 +139,37 @@ class Hand
   end
 end
 
-h1 = Hand.new([c1,c2,c3])
-puts h1.show, h1.all_ascending
-p h1.groups
+# straight_flush  = Hand.new(["AS","JS","QS","KS","TS"].map { |c| Card.new(c) })
+# four_of_a_kind  = Hand.new(["8S","8D","9S","8H","8C"].map { |c| Card.new(c) })
+# full_house      = Hand.new(["TS","3D","TH","TC","3S"].map { |c| Card.new(c) })
+# flush           = Hand.new(["3S","7S","8S","JS","AS"].map { |c| Card.new(c) })
+# straight        = Hand.new(["4C","7D","6S","5H","8C"].map { |c| Card.new(c) })
+# three_of_a_kind = Hand.new(["4H","5S","AH","5D","5C"].map { |c| Card.new(c) })
+# two_pairs       = Hand.new(["KH","TC","AD","KS","AS"].map { |c| Card.new(c) })
+# one_pair        = Hand.new(["TS","JS","TH","2C","QS"].map { |c| Card.new(c) })
+# high_card       = Hand.new(["TH","9H","6D","4S","JS"].map { |c| Card.new(c) })
 
-straight_flush  = Hand.new(["AS","JS","QS","KS","TS"].map { |c| Card.new(c) })
-four_of_a_kind  = Hand.new(["8S","8D","9S","8H","8C"].map { |c| Card.new(c) })
-full_house      = Hand.new(["TS","3D","TH","TC","3S"].map { |c| Card.new(c) })
-flush           = Hand.new(["3S","7S","8S","JS","AS"].map { |c| Card.new(c) })
-straight        = Hand.new(["4C","7D","6S","5H","8C"].map { |c| Card.new(c) })
-three_of_a_kind = Hand.new(["4H","5S","AH","5D","5C"].map { |c| Card.new(c) })
-two_pairs       = Hand.new(["KH","TC","AD","KS","AS"].map { |c| Card.new(c) })
-one_pair        = Hand.new(["TS","JS","TH","2C","QS"].map { |c| Card.new(c) })
-high_card       = Hand.new(["TH","9H","6D","4S","JS"].map { |c| Card.new(c) })
-p straight_flush.show, straight_flush.score, straight_flush.score_type
-p four_of_a_kind.show, four_of_a_kind.score, four_of_a_kind.score_type
-p full_house.show, full_house.score, full_house.score_type
-p flush.show, flush.score, flush.score_type
-p straight.show, straight.score, straight.score_type
-p three_of_a_kind.show, three_of_a_kind.score, three_of_a_kind.score_type
-p two_pairs.show, two_pairs.score, two_pairs.score_type
-p one_pair.show, one_pair.score, one_pair.score_type
-p high_card.show, high_card.score, high_card.score_type
+# test_hands = [straight_flush, four_of_a_kind, full_house, flush, straight,
+#               three_of_a_kind, two_pairs, one_pair, high_card]
+# test_hands.each do |hand|
+#   puts "(#{hand.show}), (#{hand.groups.map { |g,cs| [g,cs.length] }}), " + 
+#        "(#{hand.score}), (#{hand.score_type}), (#{hand.groups})\n---"
+# end
 
-[straight_flush, four_of_a_kind, full_house, flush, straight, three_of_a_kind,
- two_pairs, one_pair, high_card].each do |hand|
-  puts "(#{hand.show}), (#{hand.groups.map { |g,cs| [g,cs.length] }}), " + 
-       "(#{hand.score}), (#{hand.score_type}), (#{hand.groups})\n---"
-end
+# test_hands.each_cons(2) do |hand1,hand2|
+#   puts hand1 <=> hand2
+# end
 
-def comp_card(card1,card2)
-  c = (card1[0] <=> card2[0])
-  unless c == 0
-    c
-  else
-    card1[1] <=> card2[1]
-  end
-end
-
-# puts comp_card([3,"C"],[4,"H"])
-
-def comp_hand(hand1,hand2)
-  score1,score2 = [hand1,hand2].map { |hand| hand_score hand }
-  c = score1[0] <=> score2[0]
-  unless c == 0
-    c
-  else
-    high1,high2 = score1[1].zip(score2[1]).find { |high1,high2| high1 != high2 }
-    high1 <=> high2
-  end
-end
-
-def all_same_suit?(hand)
-  hand.map { |card| card[1] }.uniq.length == 1
-end
-
-def all_in_row?(hand)
-  hand.sort { |c1,c2| comp_card(c1,c2) }.each_cons(2).all? { |c1,c2|
-    c2[0] = c1[0] + 1
-  }
-end
-
-def common_cards(hand)
-  hand.group_by { |card| card[0] }.sort_by { |g,cards|
-    cards.length
-  }.reverse.map { |g,cards| cards }
-end
-
-def hand_score(hand)
-  sorted_hand = hand.sort { |c1,c2|
-    if c1[0] != c2[0]
-      c1[0] <=> c2[0]
-    else
-      c1[1] <=> c2[1]
-    end
-  }
-  high_card = sorted_hand[-1]
-  all_same_suit = all_same_suit? sorted_hand
-  all_in_row = all_in_row? sorted_hand
-  
-  # flush
-  return [9,[high_card]] if all_same_suit and all_in_row
-  common = common_cards(hand)
-  # four of a kind
-  return [8,[common[0][0],common[1][0]]] if common[0].length == 4
-  # full house
-  return [7,[common[0][0],common[1][0]]] if common[0].length == 3 and
-    common[1].length == 5
-  # flush
-  return [6,hand] if all_same_suit
-  # straight
-  return [5,[high_card]] if all_in_row
-  # three of a kind
-  return [4,common.map { |cards| cards[0] }] if common[0].length == 3
-  # two pairs
-  return [3,common.map { |cards| cards[0] }] if common[0].length == 2 and
-    common[1].length == 2
-  # one pair
-  return [2,common.map { |cards| cards[0] }] if common[0].length == 2
-  # high card
-  return [1,[high_card]]
-end
 
 hands = File.open("./p054_poker.txt").map(&:strip).map { |line|
   cards = line.split
-  cards.map! { |card|
-    [card_map[card[0]], card[-1]]
-  }
-  [cards[0...5], cards[5...10]]
+  # cards.map! { |card|
+  #   [card_map[card[0]], card[-1]]
+  # }
+  [Hand.new(cards[0...5].map { |c| Card.new(c) }),
+   Hand.new(cards[5...10].map { |c| Card.new(c) })]
 }
 
-# p hands[0]
-# p hands[1]
-
-# hands[0][0].each_cons(2) do |c1,c2|
-#   puts "c1: #{c1}, c2: #{c2}, cmp: #{comp_card(c1,c2)}"
-# end
-
-p "first in file:", hands[0][0], hands[0][1]
-p comp_hand(hands[0][0], hands[0][1])
-
-demohand1a = [[5,"H"],[5,"C"],[6,"S"],[7,"S"],[13,"D"]]
-p demohand1a
-demohand1b = [[2,"C"],[3,"S"],[8,"S"],[8,"D"],[10,"D"]]
-p "demo:", demohand1a, demohand1b, comp_hand(demohand1a,demohand1b)
-
 puts hands.count { |hand1,hand2|
-  comp_hand(hand1,hand2) == 1
+  hand1 > hand2
 }
