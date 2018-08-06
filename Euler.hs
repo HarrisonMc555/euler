59399fd41bbf7e518ab637dce20428c85edb30b6
Harrison McCullough <mccullough.harrison@gmail.com>
2018-08-06 09:50:55 -0600

    Add Euler.hs

--- Euler.hs
+++ Euler.hs
@@ -6,6 +6,10 @@ import System.Environment
 import Euler001 (solve001)
 import Euler002 (solve002)
 import Euler003 (solve003)
+import Euler004 (solve004)
+import Euler005 (solve005)
+import Euler006 (solve006)
+import Euler007 (solve007)
 
 euler :: Int -> Maybe Int
 euler = (`Data.Map.lookup` eulerSolutions)
@@ -15,4 +19,8 @@ eulerSolutions = Data.Map.fromList
   [ (1, solve001)
   , (2, solve002)
   , (3, solve003)
+  , (4, solve004)
+  , (5, solve005)
+  , (6, solve006)
+  , (7, solve007)
   ]
